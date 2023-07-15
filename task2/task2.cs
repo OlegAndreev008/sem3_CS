3609ef9c2ae1c0eeaa90d1aa682075c2ef8f430b
OlegAndreev <andreev008@mail.ru>
2023-07-15 23:24:18 +0300

    add task2

---
+++ task2/task2.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Формула нахождения расстояния - это квадр.корень из суммы квадратов разности соотв. координат двух точек.
+
+Console.Write("Введите x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.Write($"Расстояние между точками равно {Math.Round(result, 2)}");
+
+// 1. Вводим координаты первой точки 
+// 2. Вводим координаты второй точки
+// 3. В переменную result подставляем формулу
+// 4. Делаем округление результата до 2 знаков после запятой
